fbab6d690fac502493dfea2139a9aacc1eafcbac
ShariarSheikh <shariar.dev@gmail.com>
2022-07-15 07:27:44 +0600

    updated readme file

---
+++ src/App.js
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div className="container">
+      <h1 className="one">hello world</h1>
+      <h1 className="two">hello world</h1>
+      <h1 className="three">hello world</h1>
+
+      <iframe
+        className="aspect-video w-full"
+        src="https://www.youtube.com/embed/jfMHA8SqUL4"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
